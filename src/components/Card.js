dec72886701d0fdf22d650d56745136e3e6e4063
Joao Guilherme <jgmenezes@live.com>
2018-02-21 09:03:25 -0300

    Refactoring createCard function

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,7 @@ const Card = (props) => {
     return (
         <div className={`card card--${props.cardNumber}`}>
             <h1 className="card__title">{props.title}</h1>
-            <ul className={`card__list card__list--${props.cardNumber}`}>
-                <li className="list__item"></li>
-            </ul>
+            <ul className={`card__list card__list--${props.cardNumber}`}></ul>
             <a href="#" className='card__link' onClick={props.createCard}>add a card +</a>
         </div>
     )
--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Card from '../components/Card';
 
 class CardContainer extends Component {
@@ -12,24 +13,29 @@ class CardContainer extends Component {
 
     /* adicionando um item (card) na lista (interface) */
     createCard(cardNumber) {
-        const cardList = document.querySelector(`.card__list--${cardNumber}`); //adiciona no card correspondente
-        cardList.insertAdjacentHTML('beforeEnd', `<textarea cols="30" rows="4" placeholder='Descrição' class='item__input item__input--${cardNumber}--${this.state.contador}'></textarea>`); //inserindo textarea no card
-        document.querySelector(`.item__input--${cardNumber}--${this.state.contador}`).focus(); //focando no textarea após ser criado
-        this.setState({ contador: this.state.contador + 1 }); //alterando o contador para tornar a class do próximo textarea única
-    }
-
-    /* adicionando um item no array */
-    
+        const cardList = document.querySelector(`.card__list--${cardNumber}`); // card correspondente
+        const textarea = `<textarea cols="30" rows="4" placeholder='Descrição' class='item__input item__input--${cardNumber}--${this.state.contador}'></textarea>`; // textarea a ser adicionada
+        const list = document.createElement('li');
+        list.className = 'list__item';
+        list.innerHTML = textarea; //adicionando o textarea no elemento li
+        cardList.appendChild(list); //inserindo li + textarea no card
+        document.querySelector(`.item__input--${cardNumber}--${this.state.contador}`).focus(); // focando no textarea após ser criado
+        this.setState({ contador: this.state.contador + 1 }); // alterando o contador para tornar a class do próximo textarea única
+    }    
 
     render() {
+        const { items } = this.props.allItems;
+        console.log(items);
+        
         return (
             <div>
-                <Card title='TODO' cardNumber={1} createCard={() => this.createCard(1)}/>
-                <Card title='DOING' cardNumber={2} createCard={() => this.createCard(2)}/>
-                <Card title='DONE' cardNumber={3} createCard={() => this.createCard(3)}/>
+                <Card title='TODO' cardNumber={1} createCard={() => this.createCard(1)} />
+                <Card title='DOING' cardNumber={2} createCard={() => this.createCard(2)} />
+                <Card title='DONE' cardNumber={3} createCard={() => this.createCard(3)} />
             </div>
         )
     }
 }
 
-export default CardContainer
+const mapStateToProps = state => ({allItems: state.cardState})
+export default connect(mapStateToProps)(CardContainer)
